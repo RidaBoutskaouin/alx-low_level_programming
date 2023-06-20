619aee530edf843b4dd5f3bbfa98ded937c2a818
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-20 20:48:27 +0100

    check an int if its lowercase huh ?

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,13 @@
 /**
  * _islower - checks for lowercase character.
  *
+ * the parametr @c is int
+ *
  * Return: Always 0 (Success)
- * */
-
-int _islower(int c) {
+ *
+ */
+int _islower(int c)
+{
 if (c >= 97 && c <= 122)
 {
 return (1);
