f2d8d6a14ffa4ec420e779a3d6c56916a8e14288
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-20 21:37:15 +0100

    function that prints the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs  - prints the absolute value of a number
+ * @n: The number to be checked
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+return (n);
+}
+else
+{
+return (n);
+}
+return (0);
+}
