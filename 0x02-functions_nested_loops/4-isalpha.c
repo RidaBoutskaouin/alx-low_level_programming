fe610fc6e40c9724ed52c608e8a590e511e0de98
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-20 20:58:57 +0100

    check an int if its alpha ?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ *
+ * @c: The character to check
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90 ))
+{
+return (1);
+}
+return (0);
+}
