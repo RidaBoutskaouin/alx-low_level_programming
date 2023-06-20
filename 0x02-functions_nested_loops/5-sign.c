ecb1582f9753680893c145af17f178d06dfd3231
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-20 21:21:40 +0100

    function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign  - prints the sign of a number
+ *
+ * @n: The number to be checked
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+
+return (0);
+
+}
