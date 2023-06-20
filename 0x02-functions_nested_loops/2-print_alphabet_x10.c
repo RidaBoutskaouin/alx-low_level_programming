5c983f6bfd08ae53e52faf3fe0d9cba5166f646f
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-20 20:04:06 +0100

    print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet x10, in lowercase.
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 26 ; i++)
+_putchar(97 + i);
+_putchar('\n');
+}
+}
