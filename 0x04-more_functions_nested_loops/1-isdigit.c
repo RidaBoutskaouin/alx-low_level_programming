27a0b992ccee00437857be8c4498a5ba8ecc53ab
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-22 12:00:27 +0100

    function to checl if it's a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is a number, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
