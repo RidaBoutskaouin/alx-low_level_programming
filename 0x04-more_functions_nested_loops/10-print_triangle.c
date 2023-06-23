03a9217b975a2082503fe0283c0646f9f96ac13b
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-23 03:11:11 +0100

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int m, n, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (m = 1; m <= size; m++)
+	{
+		for (n = size - m; n > 0; n--)
+			_putchar(' ');
+		for (k = 1; k <= m; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
