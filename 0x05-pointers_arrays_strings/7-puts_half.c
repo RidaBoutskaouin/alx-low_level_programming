4d874c5b54ddce460887237602f2d1718b9f50a8
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-26 21:48:05 +0100

    puts only half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to prints
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	int len = 0;
+
+	while (str[len] != 0)
+	len++;
+
+	if (len % 2 == 0)
+	{
+		int half = len / 2;
+
+		for (i = half; i <= len ; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		int halfOdd = (len - 1) / 2;
+
+		for (j = 0; j <= halfOdd; j++)
+			_putchar(str[j]);
+
+	}
+	_putchar('\n');
+
+}
