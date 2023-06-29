c80d75a2afd9afb65fe3a4ebef78d41fc85e2e15
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-06-29 20:58:55 +0100

    appends not more than n charachters from src to dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - appends not more than n characters from src to dest
+ * @dest: the destination string
+ * @src: The source string
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		if (j < n)
+		dest[i + j] = src[j];
+	}
+
+	dest[i + j] = '\0';
+
+	return (dest);
+}
+
