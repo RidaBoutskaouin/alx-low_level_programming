025ac20a59ae9425c0d4dbca4850c26ad07303c3
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-01 15:09:26 +0100

    function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byte.
+ * @s: memory to be filled.
+ * @b: the char to fill with.
+ * @n: number of bytes of the memory to fill
+ * Return: the string filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x07. C - Pointers, arrays, and strings
+
+This project is part of Holberton School curriculum, and it's designed to provide a deeper understanding of pointers, arrays, and strings in C language. 
