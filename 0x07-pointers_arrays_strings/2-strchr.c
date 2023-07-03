c93911037c5de0e6e8f74279ba58648f7ebb9fb2
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-03 20:15:51 +0100

    function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,8 +13,11 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		return (s + i);
+			return (s + i);
 	}
+
+	if (s[i] == c)
+		return (s + i);
 
 	return (0);
 }
