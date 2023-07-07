bc45cb362af33744a8c30878483c8bbf4256b78a
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-07 21:41:51 +0100

    program that prints its name, followed by a new line.

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments passed
+ * @argv: array of those arguments
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf(argv[0]);
+	printf("\n");
+	return (0);
+}
