9dafc3f1f4fd68cabf4e9cf44f3f13cc683df4ce
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-13 23:13:52 +0100

    makes an array with numbers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * array_range - makes an array with numbers
+ * @min: start number
+ * @max: end number
+ *
+ * Return: pointer to arr or null
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr, i, j;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc((max - min + 1) * sizeof(int));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0, j = min; i <= (max - min); i++, j++)
+		ptr[i] = j;
+
+	return (ptr);
+}
