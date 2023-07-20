d72de08ed87b3e2ca2239d71afc75b09af6efc15
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-20 01:01:20 +0100

    adding 3-op_functions.c file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the addistion of two intgers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+
+
+/**
+ * op_sub - returns the sub of two intgers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+ * op_mul - returns the multiplication of two intgers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+
+/**
+ * op_div - returns the division of two intgers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+
+/**
+ * op_mod - returns the modulo of two intgers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
