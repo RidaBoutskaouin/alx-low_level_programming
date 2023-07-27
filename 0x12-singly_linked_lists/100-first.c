015adde35627d793112303491a68c26fcf6d2d6a
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-27 23:31:01 +0100

    function that prints something before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - prints a string before main.
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my ");
+	printf("back!\n");
+}
