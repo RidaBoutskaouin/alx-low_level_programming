7f8035f977b013448078c9992e9c79f36440132a
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-07-31 17:45:33 +0100

    fixing 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *temp;
 
+	if (head == NULL && idx != 0)
+		return (NULL);
+
 	if (idx == 0 || *head == NULL)
 	{
 
