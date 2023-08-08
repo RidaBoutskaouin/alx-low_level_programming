2cff9f34b7daa60a8f4035dab56df985a362aef2
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-08-08 11:48:11 +0100

    init & first task

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+#endif /* _MAIN_H */
+
+
