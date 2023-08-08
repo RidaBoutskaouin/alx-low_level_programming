c6654b5f6d5586c7bbc5e4166baade9de8628820
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-08-08 11:50:01 +0100

    betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,37 +7,37 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file 
+ * @filename: name of the file
  * @letters: number of letters
  * Return: actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_desc;
-    ssize_t readed, printed;
-    char *buff;
+	int file_desc;
+	ssize_t readed, printed;
+	char *buff;
 
-    if (filename == NULL)
-        return (0);
-    
-    file_desc = open(filename, O_RDONLY);
-    if (file_desc == -1)
-        return (0);
-    
-    buff = malloc(sizeof(char) * letters);
-    if (buff == NULL)
-        return (0);
-    
-    readed = read(file_desc, buff, letters);
-    if (readed == -1)
-        return (0);
-    
-    printed = write(STDOUT_FILENO, buff, readed);
-    if (printed == -1)
-        return (0);
-    
-    close(file_desc);
-    free(buff);
-    return (printed);
+	if (filename == NULL)
+		return (0);
+
+	file_desc = open(filename, O_RDONLY);
+	if (file_desc == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	readed = read(file_desc, buff, letters);
+	if (readed == -1)
+		return (0);
+
+	printed = write(STDOUT_FILENO, buff, readed);
+	if (printed == -1)
+		return (0);
+
+	close(file_desc);
+	free(buff);
+	return (printed);
 }
