5ee107189a3fd63c91ec477a993c6a916a918e84
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-09-17 15:45:19 +0100

    task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/RidaBoutskaouin/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.s
+wget -P /tmp/ https://github.com/RidaBoutskaouin/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
